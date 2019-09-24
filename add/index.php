4d65e1587afa05ab9429bf7a2fed366e91448379
mishagelka1 <mishagelka@inbox.lv>
2019-09-24 23:16:40 +0300

    A lot of self work, pride runs through me

--- add/index.php
+++ add/index.php
@@ -7,29 +7,28 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="addStyle.css">
         <!--script src="..jquery.js"></script-->
-        <script src="./script.js"></script>
+        <script src="script.js"></script>
     </head>
     <body>
-        <div class="pageContent">
+        <div class="pageContent" id="pageCont">
             <div class="topbar">
                 <h1>Product add</h1>
             </div>
 
-            <form id="form" class="addForm" action="validate.php"/>
+            <form id="form" method="POST" class="addForm" action="./validate.php">
                 <p>SKU</p> 
-                <input oninput="handleInput(this)" class="attr" type="text" name="sku" placeholder="SKU">
+                <input class="attr" type="text" name="sku" placeholder="SKU">
                 <p>Name</p> 
-                <input oninput="handleInput(this)" class="attr" type="text" name="name" placeholder="Name">
+                <input class="attr" type="text" name="name" placeholder="Name">
                 <p>Price</p>
-                <input oninput="handleInput(this)" class="attr" type="text" name="price" placeholder="Price">
+                <input class="attr" type="text" name="price" placeholder="Price">
 
                 <p>Type switcher</p>
-                <select oninput="handleInput(this)" class="attr" name="productType" id="switch">
+                <select onchange="dynamicChange()" class="attr" name="productType" id="switch">
                     <option style="display:none"></option>
-                    <option value="DVD">DVD</option>
-                    <option value="Book">Book</option>
-                    <option value="Furniture">Furniture</option>
-
+                    <option value="dvd">DVD</option>
+                    <option value="book">Book</option>
+                    <option value="furniture">Furniture</option>
                 </select>
             </form>
             <button onClick="submit()" id="applyBtn">Apply</button>
--- add/script.js
+++ add/script.js
@@ -1,4 +1,4 @@
-function handleInput(textField) {
+/*function handleInput(textField) {
     if (textField.value == "") {
         textField.style.backgroundColor = "red";
         return false;
@@ -7,28 +7,223 @@ function handleInput(textField) {
         textField.style.backgroundColor = "white";
         return true;
     }
-}
+} */
+
+ var errorMessageUp = false;
+ var dvdUp = false;
+ var bookUp = false;
+ var furnitureUp = false;
+
+ function dvdBoxUp() { // function to display dvd inputbox
+    dvdUp = true;
+    var dvdInput = document.getElementById("form");
+    var dvdBox = document.createElement("input");
+    var dvdText = document.createElement("p");
+
+    dvdText.innerText = "Enter size (in MB)";
+    dvdText.id = "did";
+
+    dvdBox.type = "text";
+    dvdBox.name = "size";
+    dvdBox.className = "attr";
+
+    dvdInput.appendChild(dvdText);
+    dvdInput.appendChild(dvdBox);
+ }
+
+ function bookBoxUp() { // function to display book inputbox
+    bookUp = true;
+    var bookInput = document.getElementById("form")
+    var bookBox = document.createElement("input");
+    var bookText = document.createElement("p");
+
+    bookText.innerText = "Enter weight (in kg)";
+    bookText.id = "bid";
+
+    bookBox.type = "text";
+    bookBox.name = "weight";
+    bookBox.className = "attr";
+
+    bookInput.appendChild(bookText);
+    bookInput.appendChild(bookBox);
+ }
+
+ function furnitureBoxUp() {
+    furnitureUp = true;
+    var furnitureInput = document.getElementById("form")
+    var furnitureBoxH = document.createElement("input");
+    var furnitureBoxW = document.createElement("input");
+    var furnitureBoxL = document.createElement("input");
+    var furnitureTextH = document.createElement("p");
+    var furnitureTextW = document.createElement("p");
+    var furnitureTextL = document.createElement("p");
+
+    furnitureTextH.innerText = "Enter height";
+    furnitureTextW.innerText = "Enter width";
+    furnitureTextL.innerText = "Enter length";
+
+    furnitureTextH.id = "hid";
+    furnitureTextW.id = "wid";
+    furnitureTextL.id = "lid";
+
+    //
+    furnitureBoxH.type = "text";
+    furnitureBoxW.type = "text";
+    furnitureBoxL.type = "text";
+    //
+    furnitureBoxH.name = "h";
+    furnitureBoxW.name = "w";
+    furnitureBoxL.name = "l";
+    //
+    furnitureBoxH.className = "attr";
+    furnitureBoxW.className = "attr";
+    furnitureBoxL.className = "attr";
+    //
+    furnitureInput.appendChild(furnitureTextH);
+    furnitureInput.appendChild(furnitureBoxH);
+    furnitureInput.appendChild(furnitureTextW);
+    furnitureInput.appendChild(furnitureBoxW);
+    furnitureInput.appendChild(furnitureTextL);
+    furnitureInput.appendChild(furnitureBoxL);
+ }
+ 
+ // Experiment for dynamic - result = success!
+ function dynamicChange() {
+
+    var form = document.getElementById("form"); // to access form elements
+
+// This section removes element (by checking if it exists) if the type chosen is being changed 
+    if(dvdUp) {
+        var deleteDVDBox = document.getElementsByName("size")[0];
+        var deleteDVDText = document.getElementById("did");
+        form.removeChild(deleteDVDBox);
+        form.removeChild(deleteDVDText);
+        dvdUp = false;
+    }
+    if (bookUp) {
+        var deleteBookBox = document.getElementsByName("weight")[0];
+        var deleteBookText = document.getElementById("bid");
+        form.removeChild(deleteBookBox);
+        form.removeChild(deleteBookText);
+        bookUp = false;
+    }
+    if(furnitureUp) {
+        var deleteHBox = document.getElementsByName("h")[0];
+        var deleteWBox = document.getElementsByName("w")[0];
+        var deleteLBox = document.getElementsByName("l")[0];
+        var deleteHText = document.getElementById("hid");
+        var deleteWText = document.getElementById("wid");
+        var deleteLText = document.getElementById("lid");
+        //
+        form.removeChild(deleteHBox);
+        form.removeChild(deleteWBox);
+        form.removeChild(deleteLBox);
+        form.removeChild(deleteHText);
+        form.removeChild(deleteWText);
+        form.removeChild(deleteLText);
+
+        furnitureUp = false;
+    }
+
+
+    var typeChosen = document.getElementById("switch");
+    var slctd = typeChosen.options[typeChosen.selectedIndex].value; //Checks which option is selected
+    if(slctd == "dvd") {
+        dvdBoxUp();
+    }
+
+    else if(slctd == "book") {
+        bookBoxUp();
+    }
+
+    else if(slctd == "furniture") {
+        furnitureBoxUp();
+    }
+
+ }
+
+// End of experiment
 
 function submit() {
     var price = document.getElementsByName("price")[0];
+    
     var correct = true;
 
     var text_elem = document.getElementsByClassName("attr");
-    console.log(text_elem);
+    //console.log(text_elem);
     for (let i = 0; i < text_elem.length; i++) {
-        if (!handleInput(text_elem[i])) correct = false;
+        if (text_elem[i].value == "") {
+            text_elem[i].style.backgroundColor = "Coral";
+            correct = false;
+        }
     }
 
     if (isNaN(price.value)) {
-        price.style.backgroundColor = "green";
+        price.style.backgroundColor = "pink";
         correct = false;
     }
+    
+    if(dvdUp) {
+        var size = document.getElementsByName("size")[0];
+        if (isNaN(size.value)) {
+            size.style.backgroundColor = "pink";
+            correct = false;
+        }
+    }
 
-    if(!correct) return;
+    else if (bookUp) {
+        var weight = document.getElementsByName("weight")[0];
+        if (isNaN(weight.value)) {
+            weight.style.backgroundColor = "pink";
+            correct = false;
+        }
+    }
+    else { // HOW TO HANDLE THIS? THERE MUST BE AN ELEGANT WAY! IT IS  NOT WORKING((((
+      /*  var dimens = [document.getElementsByName("h")[0], document.getElementsByName("w")[0], document.getElementsByName("l")[0]];
+        if(isNaN(dimens.values)) {
+            dimens.style.backgroundColor = "pink";
+            correct = false;
+        } */
+        var hbox = document.getElementsByName("h")[0];
+        var wbox = document.getElementsByName("w")[0];
+        var lbox = document.getElementsByName("l")[0];
+
+        if (isNaN(hbox.value)) {
+            hbox.style.backgroundColor = "pink";
+            correct = false;
+        }
+
+        if (isNaN(wbox.value)) {
+            wbox.style.backgroundColor = "pink";
+            correct = false;
+        }
+
+        if (isNaN(lbox.value)) {
+            lbox.style.backgroundColor = "pink";
+            correct = false;
+        }
+
+
+    }
+
+
+    if(!correct && errorMessageUp == false) // outputs an error message
+    {
+        var form = document.getElementById("form");
+        var errorMess = document.createElement("p");
+        errorMess.innerText = "Sorry, wrong input";
+        errorMess.style.marginTop = "0";
+        errorMess.style.marginBottom = "0";
+        errorMess.style.color = "red";
+        form.appendChild(errorMess);
+        errorMessageUp = true;
+        return;
+    }
+    else if(!correct && errorMessageUp == true) {
+        form.removeChild(errorMess);
+        return;
+    }
 
     var form = document.getElementById("form");
     form.submit();
-
 }
-
-window.alert('hallelouyah!');
