a2e826db8d0f835cd876108dbcd58c40ec28547d
mishagelka1 <mishagelka@inbox.lv>
2019-10-02 18:51:05 +0300

    php added

--- add/validate.php
+++ add/validate.php
@@ -1,5 +1,54 @@
 <?php
 
-var_dump($_POST)
+var_dump($_POST);
+
+$sku = $name = $price = $type = $param = "";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $sku = test_input($_POST["sku"]);
+    $name = test_input($_POST["name"]);
+    $price = test_input($_POST["price"]);
+    $productType = test_input($_POST["productType"]);
+    
+    if($_POST["productType"] == "dvd") $param = test_input($_POST["size"]);
+    else if ($_POST["productType"] == "book") $param = test_input($_POST["weight"]);
+    else {
+        $param = test_input(array($_POST["h"], $_POST["w"], $_POST["l"]));
+    }
+} 
+
+function test_input($data) {
+    if (is_array($data)) {
+        foreach($data as $value) {
+            $value = trim($value);
+            $value = stripslashes($value);
+            $value = htmlspecialchars($value);
+        }
+    }
+    else {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+    }
+        return $data;
+}
+
+
+echo "<h2>Your input:</h2>";
+echo $sku;
+echo "<br>";
+echo $name;
+echo "<br>";
+echo $price;
+echo "<br>";
+echo $productType;
+echo "<br>";
+if(is_array($param)) {
+    foreach($param as $value) echo $value . " ";
+}
+else echo $param;
 
 ?>
+
